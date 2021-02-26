a1d32d76688cbf1b319439e68864ecda565f97e9
Nourhan <nmohamed@tendegrees.sa>
2021-02-26 18:07:11 +0200

    Solution for find the smallest possible length of a (contiguous) subarray of arr, that has the same degree as arr

---
+++ CodeSignal/degreeOfArray.php
@@ -0,0 +1,29 @@
+<?php
+
+function findShortestSubArray($inputData) {
+    $arr = explode(' ', $inputData);
+    $arrDegree= -1;
+    $arr_hash = array();
+    $minLength = count($arr);
+    foreach($arr as $i => $element){
+        if(array_key_exists($element, $arr_hash)){
+            $arr_hash[$element] =  ['count' => $arr_hash[$element]['count'] + 1,
+                'firstPos' => $arr_hash[$element]['firstPos'],
+                'lastPos' => $i];
+        }else{
+            $arr_hash[$element] = ['count' => 1, 'firstPos' => $i, 'lastPos' => $i];
+        }
+    }
+    foreach($arr_hash as $key => $data){
+        $eleArrLength = $data['lastPos'] - $data['firstPos'] + 1;
+        if($data['count'] > $arrDegree){
+            $arrDegree = $data['count'];
+            $minLength = $eleArrLength;
+        }else if($data['count'] == $arrDegree && $eleArrLength < $minLength){
+            $minLength = $eleArrLength;
+        }
+    }
+    return $minLength;
+}
+
+echo findShortestSubArray('1 2 2 3 1 4 2');
